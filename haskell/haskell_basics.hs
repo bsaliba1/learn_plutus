5dba11260db163be0f174965c7ccc4026deb323a
bsaliba <baptsal@gmail.com>
2021-08-31 22:24:27 -0400

    Adding sample exercises from chapter4

--- haskell/haskell_basics.hs
+++ haskell/haskell_basics.hs
@@ -1,3 +1,4 @@
+-- Chapter 1
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
@@ -5,11 +6,42 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 length' xs = sum [1 | x <- xs]
 removeOddNumbers xxs = [ [x | x <- xs, even x] | xs <- xxs]
 removeNonUppercase :: [Char] -> [Char]
-removeNonUppercase st =
-	[c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+-- Chapter 2
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + (sum' xs)
+
+-- Chapter 4
+replicate' :: (Num i, Ord i) => a -> i -> [a]
+replicate' x y
+  | y <= 0 = []
+  | otherwise = x:replicate' x (y-1)
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n lst
+  | n <= 0 = []
+  | otherwise = x:take' (n-1) xs
+  where (x:xs) = lst
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' lst_x lst_y = (x, y):zip' xs ys
+  where (x:xs) = lst_x
+        (y:ys) = lst_y
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' n [] = False
+elem' n lst
+  | n == x = True
+  | otherwise = elem' n xs
+  where (x:xs) = lst
 
 -- Sample Problems
 -- Problem 1
@@ -28,7 +60,7 @@ mySecondLast' lst = head (reverse (init lst))
 -- Get the Kth element of a list
 elementAt :: (Num b, Ord b) => [a] -> b -> a
 elementAt lst index
-	| index == 1 = x
-	| index > 1 = elementAt xs (index-1)
-	| otherwise = error "'elementAt' cannot take a negative index"
-	where (x:xs) = lst
+  | index == 1 = x
+  | index > 1 = elementAt xs (index-1)
+  | otherwise = error "'elementAt' cannot take a negative index"
+  where (x:xs) = lst
